7dfd74ebfc7f0eb227362d3190ed6337595e2cc4
rasool <rasools27@gmail.com>
2018-08-15 11:52:52 +0530

    add file

---
+++ src/main/java/swaptwonum/SwapTwoNumbers.java
@@ -0,0 +1,28 @@
+package swaptwonum;
+
+import java.util.Scanner;
+
+public class SwapTwoNumbers 
+{
+
+		public static void main(String []s)
+		{
+			int a,b;
+			//Scanner class to read value
+			Scanner sc=new Scanner(System.in);
+
+			System.out.print("Enter value of a: ");
+			a=sc.nextInt();
+			System.out.print("Enter value of a: ");
+			b=sc.nextInt();
+
+			System.out.println("Before swapping - a: "+ a +", b: " + b);
+			////using thrid variable
+			int temp;
+			temp=a;
+			a=b;
+			b=temp;
+			//////////////////////
+			System.out.println("After swapping  - a: "+ a +", b: " + b);
+		}
+	}
---
+++ src/test/java/swaptwonum/SwapTwoNumberstest.java
@@ -0,0 +1,14 @@
+package swaptwonum;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SwapTwoNumberstest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
